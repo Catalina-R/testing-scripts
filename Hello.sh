008dfc0d8f265892455e16866cf2dca50717d4aa
Catalina Rosca <stan.catalina@gmail.com>
2019-04-19 05:49:05 +0300

    Adaugat spatii

--- Hello.sh
+++ Hello.sh
@@ -3,11 +3,11 @@
 # ZI=$(date +"%A, %T")
 #ZI=`date +"%A, %T"`
 #ZAN=$(date +"%j")
-
+echo
 echo "Este `date +"%A, %d/%m, %T"`."
-
 echo "Buna Dimineata!"
-
+echo
 echo "Este a `date +"%j"`a zi a anului."
+echo
 
 exit 0
