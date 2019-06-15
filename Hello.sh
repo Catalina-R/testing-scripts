057f47b661608d95010eeccfe021fd50b534ffee
Catalina-R <stan.catalina@gmail.com>
2019-06-15 04:28:42 +0300

    inca o incercare

--- Hello.sh
+++ Hello.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# ZI=$(date +"%A, %T")
+#ZI=$(date +"%A, %T")
 #ZI=`date +"%A, %T"`
 #ZAN=$(date +"%j")
 echo
 echo "Este `date +"%A, %d/%m, %T"`."
 echo "Buna Dimineata!"
 echo
-echo "Este a `date +"%j"`a zi a anului."
+echo "Este a `date +"%j"`a zi a anului. Ce bine!"
 echo
 echo `pwd`
 exit 0
