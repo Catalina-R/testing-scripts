312d45b242fd2dbc2123a450768b5751ee0ad67d
Catalina-R <stan.catalina@gmail.com>
2019-06-15 04:05:31 +0300

    test for github

--- drive.sh
+++ drive.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 echo "Cati ani ai?"
 read age
-if test $age -lt 19; then
+
+while [ $age -gt 100 ]
+do
+	echo "Varsta introdusa nu este valida."
+	echo "Introdu un numar intre 0 si 100."
+done
+
+read age
+
+if [ $age -lt 19 ]
+then
 	echo "Fugi de aici ca esti prea mic!"
-elif test $age -gt 65; then
+elif [ $age -gt 65 ]
+then
 	echo "Batrane, angajeaza sofer!"
 else 
 	echo "Poti sa conduci."
